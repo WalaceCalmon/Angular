d6e1bddac0806f8d14b68feb81ee0107f408611c
WalaceCalmon <walace.calmon@gmail.com>
2022-05-10 13:42:17 -0300

    OK

--- Projeto01Curso/src/app/modules/home/home.module.ts
+++ Projeto01Curso/src/app/modules/home/home.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Components
 import { HeaderComponent } from './components/header/header.component';
 import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
 import { TodoInputAddIntesComponent } from './components/todo-input-add-intes/todo-input-add-intes.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
 
 
 
@@ -12,7 +17,8 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
     HeaderComponent,
     TodoButtonDeleteAllComponent,
     TodoInputAddIntesComponent,
-    TodoListComponent
+    TodoListComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
