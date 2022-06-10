7013adfd1153d47482c3bdc8917fe77b629060f8
WalaceCalmon <walace.calmon@gmail.com>
2022-06-10 11:51:01 -0300

    Remocao

--- RotasDener/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template:`
-    <app-menu></app-menu>
-    <router-outlet></router-outlet>`
-})
-export class AppComponent {}
